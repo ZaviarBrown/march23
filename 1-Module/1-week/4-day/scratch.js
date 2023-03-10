39466245d7d1d3f92e1000aeb4eef637b9a64555
Zaviar Brown <brown.zaviar@gmail.com>
2023-03-09 22:17:38 -0800

    Day 4 lecture notes

---
+++ 1-Module/1-week/4-day/scratch.js
@@ -0,0 +1,393 @@
+// let someString = "Goodyeop0p";
+
+// function add5() {}
+
+// function add6() {
+//     return "hey";
+// }
+// function add7() {
+//     return undefined;
+// }
+
+// // console.log(add5);
+// // console.log(add5()); console.log(add6());
+// // console.log(add7());
+// // console.log(someString);
+// // add5();
+
+// let myStr = "hey";
+// let myOtherString = add6();
+// console.log(myStr);
+// console.log(myOtherString);
+// // add7();
+
+// function myFunctionName() {
+//     console.log("Hey I ran");
+
+//     return "Hey I got returned";
+
+//     console.log("No one will ever see me :( ");
+// }
+
+// console.log(myFunctionName());
+
+// let number =
+
+// function add5(number) {
+//     let sum = number + 5;
+
+//     return sum;
+// }
+
+// console.log(add5(10));
+// console.log(add5(5));
+// console.log(add5(2500));
+// console.log(add5(2333));
+// console.log(add5(2333));
+// console.log(add5(2333));
+// console.log(add5(2333));
+// console.log(add5(2333));
+// console.log(add5(2333));
+// console.log(add5(2333));
+// console.log(add5(2333));
+// console.log(add5(2333));
+// console.log(add5(2333));
+// console.log(add5(2333));
+// console.log(add5(2333));
+// console.log(add5(2333));
+// console.log(add5(2333));
+// console.log(add5(2333));
+
+// function addANumberToAnother(lowNum, highNum) {
+//     // let sum = lowNum + highNum;
+
+//     // return sum;
+//     console.log(lowNum, "THIS IS THE LOWWWW NUMMMM");
+// }
+
+// // console.log(addANumberToAnother(55, 27));
+// // console.log(addANumberToAnother(27, 55));
+// addANumberToAnother(27, 55);
+// addANumberToAnother(55, 27);
+
+// function myFunc(someVal) {
+//     return console.log(someVal);
+// }
+
+// console.log(myFunc("Heyoooo"));
+
+// let myVal = console.log()
+
+// console.log(myVal)
+
+// let c = "turtle";
+
+// if (c === "dog") {
+//     console.log("I'm a dog!");
+// } else if (c === "turtle") {
+//     console.log("I'm a turtle!");
+// } else if (c === "turtle") {
+//     console.log("I'm a potato");
+// } else { }
+
+// let num = 5;
+// let biggerNum = 10;
+
+// if (num < 9) {
+//     console.log("that's a small num", num);
+// }
+
+// if (biggerNum < 9) {
+//     console.log("that's a small num", biggerNum);
+// }
+
+// let c = "turtle";
+// // let c = undefined;
+// if (c === "dog") {
+//     console.log("I'm a dog!");
+// } else if (c === "turtle") {
+//     console.log("I'm a turtle!");
+// } else if (c === "potato") {
+//     console.log("I'm a potato");
+// } else {
+//     console.log("I don't know what I am");
+// }
+
+// let c = "turtle";
+
+// if (c === "dog") {
+//     console.log("I'm a dog!");
+// }
+// if (c === "turtle") {
+//     console.log("I'm a turtle!");
+// }
+// if (c === "turtle") {
+//     console.log("I'm a potato");
+// }
+
+// let counter = 0;
+
+// while (counter < 5) {
+//     // thousands of lines
+//     // thousands of lines
+//     // thousands ogf lines
+//     console.log("hello world");
+//     counter++; // 0 | 1 | 2
+//     // thousands of lines
+//     // thousands of lines
+//     // thousands of lines
+// }
+
+// console.log("goodbye world");
+
+// let i = 0;
+
+// while (i < 300) {
+//     console.log("---------------Start of Loop---------------");
+//     console.log("hello world");
+//     i++;
+//     console.log("New value of i:", i);
+
+//     if (i === 222) {
+//         console.log(
+//             "--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+//         );
+//     }
+//     console.log("---------------End of Loop---------------");
+// }
+
+// let i = 1;
+
+// while (i <= 5) {
+//     console.log("---------------Start of Loop---------------");
+//     console.log("hello world #" + i );
+//     i++;
+//     if (i === 3) {
+//         console.log("This is the third loop");
+//     }
+//     // i++;
+//         console.log("---------------End of Loop---------------");
+// }
+
+// console.log("---------------While Loop---------------");
+
+// let counter = 0;
+
+// while (counter < 5) {
+//     console.log("hello world");
+//     counter++;
+// }
+
+// console.log("---------------For Loop---------------");
+
+// //  (the counter you create ; the condition you check ; add/subtract your counter)
+// let counter = 0;
+
+// for ( ; counter < 5; ) {
+//     console.log("hello world");
+//     counter++;
+// }
+
+// for (let counter = 0; counter < 5; counter++) {
+//     console.log("hello world");
+// }
+
+// Write a function printFives(max) that prints out the multiples of 5 that are
+// less than max.
+// Try to solve this in two ways, one using
+// an if statement, and one without.
+
+// //? No if statement version
+// function printFives(max) {
+//     for (let i = 0; i < max; i += 5) {
+//         console.log(i)
+//     }
+// }
+
+// //? If statement version
+
+// function printFives(max) {
+//     for (let i = 0; i < max; i++) {
+//         //? Version 1
+//         if (i % 5 === 0) {
+//             console.log(i);
+//         }
+//         //? Version 2
+//         if (i % 5) {
+//         } else {
+//             console.log(i);
+//         }
+//         //? Version 3
+//         if (!(i % 5)) {
+//             console.log(i);
+//         }
+//     }
+// }
+
+// printFives(20); // prints out:
+// 0
+// 5
+// 10
+// 15
+
+// printFives(50);
+
+// console.log(5 % 1);
+// console.log(5 % 2);
+// console.log(5 % 3);
+// console.log(5 % 4);
+// console.log(5 % 5);
+// console.log(5 % 6);
+// console.log(5 % 7);
+// console.log(5 % 8);
+// console.log(5 % 9);
+// console.log(5 % 10);
+
+// let forks = "forks";
+// let knives = "knives";
+// let spoons = "spoons";
+
+// let kitchenDrawer = ["forks", "knives", "spoons", 5, "Zaviar"];
+
+// // console.log(forks);
+// // console.log(knives);
+// // console.log(spoons);
+
+// console.log(kitchenDrawer);
+
+// let arr = [1, "hello", false, NaN, [1, 2, 3]];
+
+// let someNum = 320;
+
+// let firstName = "Zaviar";
+
+// let myArr = ["momo", someNum, [firstName, "Brown"]];
+
+// console.log(myArr);
+
+// // // firstName = "Zaviar"
+// // var firstName = "Zaviar";
+// let firstName = "Zaviar";
+// let arr = ["Zaviar", 'r'];
+// let kitchenDrawer = ["forks", "knives", 'r', "spoons", 5, "Zaviar"];
+// let firstNameArr = ["Z", "a", "v", "i", "a", "r"];
+
+// console.log(firstName.indexOf("a"));
+// console.log(arr.indexOf("r"));
+// console.log(kitchenDrawer.indexOf("r"));
+// // console.log(firstNameArr.indexOf());
+
+// // console.log(firstName[1])
+// // console.log(arr[1])
+// // console.log(firstNameArr[1]);
+
+// // console.log(firstName.length);
+// // // console.log(arr.length);
+// // // console.log(firstNameArr.length);
+
+// let array1 = [1, 2, 3];
+// let array2 = [4, 5, 6];
+// let array3 = [7];
+
+// // let array4 = array1.concat(array2);
+
+// // let array4 = array1.concat(array2, array3);
+// // let array4 = array2.concat(array1);
+// let array4 = array3.concat(array1, array2);
+// // let array4 = array1.concat(array2, array3);
+
+// // console.log(array1, "array 1");
+// // console.log(array2, "array 2");
+// console.log(array4, "array 4");
+
+// function printFives(max) {
+//     // console.log("Max is", max);
+
+//     // console.log("We're entering the loop for the first time!!!!");
+
+//     for (let i = 0; i < max; i++) {
+//         // console.log("---------------Start of Loop---------------");
+//         // console.log("i is currently:", i);
+
+//         if (i % 5 === 0) {
+//             // console.log("If condition was truthy!!!");
+//             console.log(i);
+//         }
+
+//         // console.log("---------------End of Loop---------------");
+//     }
+
+//     // console.log("I'm all done looopin");
+// }
+
+// printFives(20);
+
+// function mystery(array) {
+//     let value = array[0];
+//     console.log("This is value", value);
+
+//     for (let i = 1; i < array.length; i += 1) {
+//         console.log("---------------Start of Loop---------------");
+
+//         let num = array[i];
+//         console.log("num:", num);
+
+//         console.log("value before multiplying", value);
+//         console.log("value * num", value, "*", num, value * num);
+//         value *= num;
+//         console.log("value after multiplying", value);
+
+//         console.log("------------------------------");
+//     }
+
+//     console.log("This is the final value", value);
+
+//     return value;
+// }
+
+// console.log(mystery([1, 2, 3, 4]));
+
+// function mystery(array) {
+//     let value = array[0];
+
+//     for (let i = 1; i < array.length; i += 1) {
+//         let num = array[i];
+//         value *= num;
+//     }
+
+//     return value;
+// }
+
+// console.log(mystery([1, 2, 3, 4]));
+
+// function mystery(array) {
+//     let value = 1;
+//     for (let i = 0; i < array.length; i += 1) {
+//         let num = array[i];
+//         value *= num;
+//     }
+
+//     return value;
+// }
+
+// console.log(mystery([1, 2, 3, 4]));
+
+// Write a function threeOrSeven that takes in a number and returns true if the
+// number is divisible by either 3 or 7 and false otherwise.
+
+// Write two versions of this function, using conditionals (if) and without
+// using conditionals.
+
+function threeOrSeven(num) {
+    if (num % 3 === 0) {
+        return true;
+    } else if (num % 7 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(threeOrSeven(3)); // => true
+console.log(threeOrSeven(42)); // => true
+console.log(threeOrSeven(8)); // => false
