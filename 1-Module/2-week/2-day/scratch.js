77c4199394535390b2d48f5ed90a742b94ef2cb6
Zaviar Brown <brown.zaviar@gmail.com>
2023-03-14 20:02:16 -0700

    Slice splice split join lecture notes

---
+++ 1-Module/2-week/2-day/scratch.js
@@ -0,0 +1,62 @@
+// let cats = ["Momo", "Tenten", "Kiki"];
+// let newKitties = cats.slice(1);
+// console.log(cats, "cats after slice");
+// console.log(newKitties, "newKitties after slice");
+
+// let cats = ["Momo", "Tenten", "Kiki", "Garfield"];
+// let newKittiess = cats.splice(1, 2);
+// cats.splice(1, 2);
+// console.log(cats, "cats after SPLICE");
+// console.log(newKittiess, "newKittiess after SPLICE");
+
+// console.log(cats);
+// console.log(newKitties);
+
+// let cats = ["Momo", "Tenten", "Kiki"];
+
+// // let withoutSlice = cats;
+
+// // cats.push("Zaviar");
+
+// // console.log(cats, "original cats");
+// // console.log(withoutSlice, "withoutSlice");
+
+// let slicedKitties = cats.slice();
+
+// // cats.push("Zaviar");
+// slicedKitties.push("Zaviar");
+
+// console.log(cats, "original cats");
+// console.log(slicedKitties, "slicedKitties");
+
+// let str = "Z";
+
+// let otherStr = str;
+
+// otherStr = "hey";
+
+// console.log(otherStr, str)
+
+// let sentenceArray = [
+//     "The",
+//     "quick",
+//     "brown",
+//     "fox",
+//     "jumps",
+//     "over",
+//     "the",
+//     "lazy",
+//     "dog.",
+// ];
+
+let str = "The quick brown fox jumps over the lazy dog.";
+
+let sentenceArray = str.split(" ");
+
+// console.log(sentenceArray)
+
+// console.log(sentenceArray.join(", "))
+
+// let phoneNum = [555, 555, 5555]
+
+console.log(sentenceArray.join("_/?<)))"));
