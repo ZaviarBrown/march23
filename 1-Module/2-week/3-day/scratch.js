685bec5733821eede091b5e5680fb24ac6bcffb2
Zaviar Brown <brown.zaviar@gmail.com>
2023-03-15 15:57:19 -0700

    Helper function lecture notes

---
+++ 1-Module/2-week/3-day/scratch.js
@@ -0,0 +1,86 @@
+// // Does this given word contain the letter e?
+// let hasE = function (word) {
+//     if (word.toLowerCase().includes("e")) {
+//         return true;
+//     }
+
+//     return false;
+// };
+
+// // Remove words with e
+// let loopAndSplice = function (array) {
+//     for (let i = 0; i < array.length; i++) {
+//         let currWord = array[i];
+
+//         if (hasE(currWord)) {
+//             array.splice(i, 1);
+//             i--;
+//         }
+//     }
+
+//     return array;
+// };
+
+// // Removes all words from the sentence with the letter "e"
+// let removeEWords = function (sentence) {
+//     let wordArr = sentence.split(" ");
+
+//     let newArr = loopAndSplice(wordArr);
+
+//     return newArr;
+// };
+
+// // console.log(hasE("Zaviar")); // false
+// // console.log(hasE("Tenten")); // true
+// // console.log(hasE("Momo")); // false
+// // console.log(hasE("Tiger")); // true
+// // console.log(hasE("EGG")); // true
+
+// console.log(removeEWords("What time hey is it everyone?")); // 'What is it'
+// console.log(removeEWords("ENTER the building")); // 'building'
+
+// let yell = function (str) {
+//     console.log("This is yell running");
+
+//     return str.toUpperCase();
+// };
+
+// // console.log(yell("Zaviar"));
+
+// let whisper = function (string) {
+//     console.log("This is whisper running");
+
+//     return string.toLowerCase();
+// };
+
+// let addDots = function (strArray) {
+//     console.log("This is addDots running");
+
+//     return strArray.join(" ... ");
+// };
+
+// // Returns a string "echo-ized" with addDots
+// let echo = function (banana) {
+//     console.log("This is echo running");
+//     return addDots([yell(banana), banana, whisper(banana)]);
+// };
+
+// let echoWithNoHelp = function (string) {
+//     return (
+//         string.toUpperCase() + " ... " + string + " ... " + string.toLowerCase()
+//     );
+// };
+
+// console.log(echo("Mom!")); // => prints "MOM! ... Mom! ... mom!"
+// console.log(echo("hey")); // => prints "HEY ... hey ... hey"
+// console.log(echo("JUMp")); // => prints "JUMP ... JUMp ... jump"
+
+// let echoWithoutAddDots = function (banana) {
+//     return yell(banana) + " ... " + banana + " ... " + whisper(banana);
+// };
+
+// let printTheThing = function (aThing) {
+//     console.log(aThing);
+// };
+
+// printTheThing(5 + 5)
