33a0fbc003e1db6a1b11968829f2933ba401a602
Zaviar Brown <brown.zaviar@gmail.com>
2023-03-21 16:07:35 -0700

    Arrow function lecture

---
+++ 1-Module/3-week/2-day/scratch.js
@@ -0,0 +1,53 @@
+// function myFavGame() {
+//     return "Elden Ring";
+// }
+
+// let myFavGame = () => "Elden Ring";
+
+// let arr = [1, 2, 3];
+
+// let i = 0;
+
+// while (i < arr.length) {
+//     console.log(arr[i]);
+//     i++;
+// }
+
+// let myFavGame = function (gameName) {
+//     return gameName;
+// };
+
+// let myFavGame = (gameName) => {
+//     return gameName;
+// };
+
+// function myFavGame(gameName) {
+//     return gameName;
+// }
+
+// let myFavGame = (gameName) => {
+//     // All sorts of functiony things
+//     // All sorts of functiony things
+//     // All sorts of functiony things
+//     // All sorts of functiony things
+//     // All sorts of functiony things
+//     // All sorts of functiony things
+//     // All sorts of functiony things
+//     return gameName;
+// }
+
+
+
+// console.log(myFavGame("Elden Ring"));
+
+// let myFavGame = function () {
+
+// }
+
+// Anonymous callback
+let arr = [1, 2, 3];
+let addOne = arr.map(function () {
+    return "hey";
+});
+let addOtherOne = arr.map(() => "hey");
+console.log(addOne);
