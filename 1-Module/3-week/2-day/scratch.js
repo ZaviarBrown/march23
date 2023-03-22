3ee7df31274a3bd31cd19a9c1af0cf9ee41213bd
Zaviar Brown <brown.zaviar@gmail.com>
2023-03-21 20:57:47 -0700

    Arrow function lecture example

--- 1-Module/3-week/2-day/scratch.js
+++ 1-Module/3-week/2-day/scratch.js
@@ -1,1 +1,47 @@
-console.log("NOT heyoooo");
+// function myFavGame() {
+//     return "Elden Ring";
+// }
+
+// let myFavGame = function () {
+//     return "Elden Ring";
+// };
+
+// let myFavGame = () => {
+//     return "Elden Ring";
+// };
+
+// let myFavGame = () => "Elden Ring"
+
+// let myFavGame = () => {
+//     return "Elden Ring";
+// }
+
+// let myFavGame = (str) => {
+//     return str;
+// };
+
+// console.log(myFavGame("Elden Ring"));
+
+// let arr = [1, 2, 3]
+
+// let i = 0;
+
+// while (i < arr.length) {
+//     console.log(arr[i])
+
+//     i++
+// }
+
+// let arr = [1, 2, 3];
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+// Anonymous callback
+// let arr = [1, 2, 3];
+// let addOne = arr.map((num) => num + 1);
+// let addOneOther = arr.map(function (num) {
+//     return num + 1;
+// });
+// let addOtherOne = arr.splice(1, 2, "hello");
+// console.log(addOne);
