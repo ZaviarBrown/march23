cc03bc9b5f720a2035436ce0df94b2395a3b91ba
Bill Adams <vikingbill@gmail.com>
2023-03-22 22:03:03 -0400

    end class

---
+++ 1-Module/3-week/3-day/pojoPracticePt1/problems/02-values-in-object.js
@@ -0,0 +1,29 @@
+/***********************************************************************
+Write a function called `valuesInObject(obj)` that takes in an object and returns 
+an array of all the values within that Object. 
+
+
+Do this once using using a `for...in` loop and once using `Object.values`.
+
+
+Examples:
+
+let animals = {dog: "Wolfie", cat: "Jet", bison: "Bilbo"}
+let foods = {apple: "tart", lemon: "sour", mango: "sweet"}
+valuesInObject(animals); // => ["Wolfie", "Jet", "Bilbo"]
+valuesInObject(foods); // => ["tart", "sour", "sweet"]
+***********************************************************************/
+
+function valuesInObject(obj) {
+  // Your code here
+  // return Object.values(obj);
+  let vals = [];
+  for (let key in obj){
+    let value = obj[key];
+    vals.push(value)
+  }
+  return vals;
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = valuesInObject;
--- 1-Module/3-week/3-day/scratch.js
+++ 1-Module/3-week/3-day/scratch.js
@@ -1,33 +1,70 @@
-// Object?
+// // Object?
 
-// string -> 
+// // string -> 
 
-// "hello"[0]-> 'h'
-// ["h","e","l","l","o"][0] -> "h" 
+// // "hello"[0]-> 'h'
+// // ["h","e","l","l","o"][0] -> "h" 
 
-let myObj = {
-    "h" :"h",
-    "e" : "e",
-    "l" : "l",
-    "l" : "overwritten",
-    "o" : "o",
-    "length": "banana"
-}
-
-// console.log([] instanceof Object)
-
-// let wayne = {
-//     furColor: "black",
-//     hasTail : false,
-//     isChonky : true,
-//     cuddly : true,
-//     age : 2,
+// let myObj = {
+//     "h" :"h",
+//     "e" : "e",
+//     "l" : "l",
+//     "l" : "overwritten",
+//     "o" : "o",
+//     "length": "banana"
 // }
 
-let wayne = {age:2};
-wayne.hasTail = false;
-console.log(wayne) 
-wayne["cuddly"] = true;
-let chonkitude = "isChonky";
-wayne[chonkitude] = true;
-console.log(wayne)
+// // console.log([] instanceof Object)
+
+// // let wayne = {
+// //     furColor: "black",
+// //     hasTail : false,
+// //     isChonky : true,
+// //     cuddly : true,
+// //     age : 2,
+// // }
+
+// let wayne = {age:2};
+// wayne.hasTail = false;
+// // console.log(wayne) 
+// wayne["cuddly"] = true;
+// let chonkitude = "isChonky";
+// wayne[chonkitude] = true;
+// // console.log(wayne)
+
+// // ITERATING THROUGH OBJECTS
+
+// // for...in
+// //     key(s) object
+// console.table(wayne)
+// for(let key in wayne){
+//     console.log(key)
+//     let value = wayne[key];
+//     console.log("    ", value);
+// }
+
+// let keys = Object.keys(wayne)
+
+// console.log(keys)
+// for (let idx = 0; idx < keys.length; idx++){
+//     let key = keys[idx];
+//     let value = wayne[key];
+//     console.log("At the key of", key,"you can find the value", value)
+// }
+
+
+let wayne = {
+    furColor: "black",
+    hasTail: false,
+    isChonky: true,
+    cuddly: true,
+    age: 2,
+};
+
+let values = Object.values(wayne);
+
+console["log"](values);
+
+let entries = Object.entries(wayne);
+console["table"](entries);
+console["log"](entries);
