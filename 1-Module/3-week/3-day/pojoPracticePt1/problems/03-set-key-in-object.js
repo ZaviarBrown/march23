853c74afcfb04f3b0519da13fa286c4c451365e7
Bill Adams <vikingbill@gmail.com>
2023-03-23 17:44:34 -0400

    add pojo practice pt1 solve

---
+++ 1-Module/3-week/3-day/pojoPracticePt1/problems/03-set-key-in-object.js
@@ -0,0 +1,25 @@
+/***********************************************************************
+Write a function `setKeyInObject(obj, string, value)` that takes in three 
+parameters. The first parameter is an object, the second parameter will be a 
+string and the third parameter will be a value. Your job is to return the object
+adding the second parameter as a key using the third parameter as its value.
+
+Examples:
+
+let obj = {}
+setKeyInObject(obj, "apple", "yum"); // => {apple: "yum"}
+
+let obj1 = {str: "hello"}
+setKeyInObject(obj1, "num", 3); // => {str: "hello", num: 3}
+***********************************************************************/
+
+function setKeyInObject(obj, string, value) {
+  // Your code here
+  // eval(`obj.${string} = value;`) // Take a look at eval on MDN to find 
+                                    // out more!
+  obj[string] = value;
+  return obj;
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = setKeyInObject;
