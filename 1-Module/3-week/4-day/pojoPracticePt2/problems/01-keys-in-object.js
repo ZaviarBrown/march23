6b6f9714eeefb58a623c4f62ae565899c4976785
Bill Adams <vikingbill@gmail.com>
2023-03-25 15:05:30 -0400

    add pojo solve

---
+++ 1-Module/3-week/4-day/pojoPracticePt2/problems/01-keys-in-object.js
@@ -0,0 +1,26 @@
+/***********************************************************************
+Write a function called `keysInObject(obj)` that takes in an object and returns 
+an array of all the keys within that Object.
+
+Do this once using using a `for...in` loop and once using `Object.keys`.
+
+Examples:
+
+let animals = {dog: 'Wolfie', cat: 'Jet', bison: 'Bilbo'}
+let foods = {apple: 'tart', lemon: 'sour', mango: 'sweet'}
+keysInObject(animals); // => ["dog", "cat", "bison"]
+keysInObject(foods); // => ["apple", "lemon", "mango"]
+***********************************************************************/
+
+function keysInObject(obj) {
+  // Your code here
+  // return Object.keys(obj);
+  let keys = [];
+  for (let key in obj){
+    keys.push(key);
+  }
+  return keys;
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = keysInObject;
---
+++ 1-Module/3-week/4-day/pojoPracticePt2/problems/13-string-converter.js
@@ -0,0 +1,28 @@
+/***********************************************************************
+Write a function `stringConverter(string)` that will intake a
+string as an argument and returns an object representing the count of each
+character in the string. **Hint:** don't forget you can check if a key is present
+in an object by using `obj[key] === undefined`.
+
+Examples:
+
+console.log(stringConverter("apple")); // => {a: 1, p: 2, l: 1, e: 1}
+console.log(stringConverter("banana")); // => {b: 1, a: 3, n: 2}
+console.log(stringConverter("raccoon")); // => {r: 1, a: 1, c: 2, o: 2, n: 1}
+***********************************************************************/
+
+function stringConverter(string) {
+  // Your code here
+  let count = {};
+  for (let char of string){
+    if (count[char] === undefined){
+      count[char] = 1;
+    } else {
+      count[char]++;
+    }
+  }
+  return count;
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = stringConverter;
