0056a8a36efff66d9abfcd1e3bee06f440c88563
Zaviar Brown <brown.zaviar@gmail.com>
2023-04-13 16:58:32 -0700

    Flatten

--- 1-Module/5-week/4-day/recursionPractice_pt3/problems/11-flatten.js
+++ 1-Module/5-week/4-day/recursionPractice_pt3/problems/11-flatten.js
@@ -10,12 +10,108 @@ Examples:
 flatten([]); // []
 flatten([1, 2]); // [1, 2]
 flatten([1, [2, [3]]]); // [1, 2, 3]
+
+//? Oldie but a goodie
+for (let i = 0; i < arr.length; i++) {
+        const el = arr[i];
+
+        if (Array.isArray(el)) {
+            // Recursive Step & Case
+            const recReturn = flatten(el);
+            finalArr.push(...recReturn);
+        } else {
+            finalArr.push(el);
+        }
+    }
+
+//? For Each version
+    arr.forEach((el) => {
+        if (Array.isArray(el)) {
+            // Recursive Step & Case
+            const recReturn = flatten(el);
+            finalArr.push(...recReturn);
+        } else {
+            finalArr.push(el);
+        }
+    });
 ***********************************************************************/
 
-function flatten(arr) {
-  // Your code here
-}
-  
+/**
+ * I need to find something that is NOT an array
+ * Array.isArray(thisIsAnArrayOrNot)
+ */
+
+const flatten = (arr) => {
+    // Base Case
+    if (!arr.length) {
+        return [];
+    }
+
+    const finalArr = [];
+
+    arr.forEach((el) => {
+        if (Array.isArray(el)) {
+            // Recursive Step & Case
+            const recReturn = flatten(el);
+            finalArr.push(...recReturn);
+        } else {
+            finalArr.push(el);
+        }
+    });
+
+    return finalArr;
+};
+
+// const flatten = (arr) =>
+//     arr.length
+//         ? Array.isArray(arr[0])
+//             ? flatten(arr[0]).concat(...flatten(arr.slice(1)))
+//             : [arr[0], ...flatten(arr.slice(1))]
+//         : [];
+
+// //! The Ugliest Solution? Probably. Does it work? Yes :)
+// function flatten(arr) {
+//     let flat = []; //store result
+//     if (arr.length === 0) {
+//         //base case
+//         return arr;
+//     }
+//     //recursive step/case????
+//     arr.forEach((item) => {
+//         if (Array.isArray(item)) {
+//             flat.push(...flatten(item));
+//         } else {
+//             flat.push(item);
+//         }
+//     });
+
+//     return flat;
+// }
+
+// //! Terrible awful one line don't do this it's bad
+// function flatten(arr) {
+//     let newArr = [];
+//     if (arr.length === 0) return [];
+//     arr.forEach((ele) =>
+//         Array.isArray(ele)
+//             ? (newArr = [...newArr, ...flatten(ele)])
+//             : newArr.push(ele)
+//     );
+//     return newArr;
+// }
+
+// function flatten(arr) {
+//     if (arr.length === 0) {
+//         return [];
+//     } else {
+//         if (Array.isArray(arr[0])) {
+//             return [...flatten(arr[0]), ...flatten(arr.slice(1))];
+//             // return flatten(arr[0]).concat(flatten(arr.slice(1)));
+//         } else {
+//             return [arr[0], ...flatten(arr.slice(1))];
+//         }
+//     }
+// }
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = flatten;
-  
