a770e1105baffb66cab4ebfa057cbcf3c3757c8f
Zaviar Brown <brown.zaviar@gmail.com>
2023-04-22 14:38:29 -0700

    Another solution to recursive sort

--- 1-Module/6-week/5-day/additionalProblems/problems/02-recursive-sort.js
+++ 1-Module/6-week/5-day/additionalProblems/problems/02-recursive-sort.js
@@ -1,7 +1,8 @@
 /*
     Recursive Sort
 
-    Given an array of numbers, write a function that returns a new array of numbers sorted from lowest to highest.
+    Given an array of numbers, write a function that returns a new array of 
+    numbers sorted from lowest to highest.
 
     console.log(sort([])) // prints []
     console.log(sort([9])) // prints [9]
@@ -9,21 +10,35 @@
     console.log(sort([14, 5, 10, 6, 3, 4, 21, 16, 9])); // prints [ 3, 4, 5, 6, 9, 10, 14, 16, 21 ]
 */
 
-function sort(nums) {
-    if (nums.length <= 1) {
-        return nums;
-    }
-    let index = 0;
-    let lowest = nums[0];
-    for (let i = 1; i < nums.length; i++) {
-        if (nums[i] < lowest) {
-            index = i;
-            lowest = nums[i];
-        }
-    }
-    const firstPart = nums.splice(index, 1);
-    return firstPart.concat(sort(nums));
-}
+const sort = (nums) => {
+    if (!nums.length) return [];
+
+    const smallestNum = nums.reduce((smallestNum, nextNum) => {
+        if (smallestNum > nextNum) {
+            return nextNum;
+        } else return smallestNum;
+    });
+
+    const otherNums = nums.filter((num) => num !== smallestNum);
+
+    return [smallestNum, ...sort(otherNums)];
+};
+
+// function sort(nums) {
+//     if (nums.length <= 1) {
+//         return nums;
+//     }
+//     let index = 0;
+//     let lowest = nums[0];
+//     for (let i = 1; i < nums.length; i++) {
+//         if (nums[i] < lowest) {
+//             index = i;
+//             lowest = nums[i];
+//         }
+//     }
+//     const firstPart = nums.splice(index, 1);
+//     return firstPart.concat(sort(nums));
+// }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
