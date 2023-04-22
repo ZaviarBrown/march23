b108e8ddf2485847a738beae22efe780a6ac642b
Zaviar Brown <brown.zaviar@gmail.com>
2023-04-22 13:49:00 -0700

    PA walkthrough

---
+++ 1-Module/6-week/5-day/pracAss_5-6_pt2/problems/02-print-and-pause.js
@@ -0,0 +1,47 @@
+/***********************************************************************
+Given a list of positive integers, print each integer and pause for that
+many milliseconds before printing the next one. Make sure you use a recursive
+approach to solve this problem.
+
+Example:
+
+printAndPause([200, 800, 200, 800, 200, 800])
+200
+// pause 200ms
+800
+// pause 800ms
+200
+// pause 200ms
+800
+// pause 800ms
+200
+// pause 200ms
+800
+// pause 800ms
+
+Run tests on just this file by typing `npm test test/02-print-and-pause-spec.js`
+on the command line.
+
+***********************************************************************/
+
+function printAndPause(nums) {
+    // Base Case
+    if (!nums.length) return "Finished!";
+
+    // Recursive Step
+    const currNum = nums.shift();
+    console.log(currNum);
+
+    // Recursive Case
+    // setTimeout(printAndPause, currNum, nums);
+    setTimeout(() => {
+        printAndPause(nums);
+    }, currNum);
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+try {
+    module.exports = printAndPause;
+} catch {
+    module.exports = null;
+}
