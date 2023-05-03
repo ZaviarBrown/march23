d33790479ca2cecac748af964c9fe94d31292751
Zaviar Brown <brown.zaviar@gmail.com>
2023-05-02 19:54:09 -0700

    Context be crazy

---
+++ 2-Module/2-week/2-day/README.md
@@ -0,0 +1,143 @@
+# M2W2D2 - 2's day!!!
+
+## Context is determined by WHERE a function is called
+
+### The problem
+
+```js
+class Cat {
+  speak(sound = "meow") {
+    // console.log(this);
+    console.log(sound);
+  }
+
+  speakSpecific(sound) {
+    // console.log(this);
+    this.speak(sound);
+  }
+
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+let cat = new Cat("cat");
+let otherCat = new Cat("otherCat");
+
+cat.speak(); // 'meow'
+cat.speakSpecific("ayyeee lmao"); // 'ayyeee lmao'
+
+otherCat.speak(); // 'meow'
+otherCat.speakSpecific("ayyeee lmao"); // 'ayyeee lmao'
+
+const mySpeak = cat.speak;
+const mySpecific = cat.speakSpecific;
+
+console.log(mySpeak); // [Function: speak]
+console.log(mySpecific); // [Function: speakSpecific]
+
+mySpeak("still works"); // 'still works'
+mySpecific("wait what happened"); // TypeError
+```
+
+### The solution?
+
+### `Bind`, `Call` / `Apply`, & sometimes `Arrow Functions`
+
+---
+
+## Bind
+
+Receives an object to use as the context
+
+Then `returns a new function` with that bound context
+
+- Can't be unbound/rebound!
+
+```js
+let cat = new Cat("cat");
+
+const mySpecific = cat.speakSpecific;
+const boundSpecific = mySpecific.bind(cat);
+
+console.log(boundSpecific); // [Function: bound speakSpecific]
+boundSpecific("ggez"); // ggez
+
+const boundForever = mySpecific.bind(cat, "I'll never die");
+boundForever("hey");
+boundForever("ggez");
+```
+
+## Call & Apply
+
+Receives an object to use as the context
+
+Then `invokes the function` with that bound context
+
+- `C`all
+  - Receives `c`omma separated arguments
+- `A`pply
+  - Receives an `a`rray as an argument
+
+```js
+let cat = new Cat("cat");
+
+const mySpecific = cat.speakSpecific;
+mySpecific.call(cat); // meow
+mySpecific.apply(cat); // meow
+
+mySpecific.call(cat, "grrrrr"); // grrrrr
+mySpecific.apply(cat, ["hisssss"]); // hisssss
+```
+
+## Arrows be crazy
+
+Your assessment doesn't care about the intricacies of arrow function context
+
+**_So Neither Should You_**
+
+Only thing to note
+
+```js
+class Cat {
+  speak(sound = "meow") {
+    console.log(sound);
+  }
+
+  speakSpecific(sound) {
+    this.speak(sound);
+  }
+
+  speakLater(delay) {
+    setTimeout(function () {
+      this.speak();
+    }, delay);
+  }
+}
+let cat = new Cat();
+
+cat.speakLater(1000); // TypeError
+```
+
+So let arrow functions save the day!
+
+```js
+class Cat {
+  speak(sound = "meow") {
+    console.log(sound);
+  }
+
+  speakSpecific(sound) {
+    this.speak(sound);
+  }
+
+  speakLater(delay) {
+    setTimeout(() => {
+      this.speak();
+    }, delay);
+  }
+}
+let cat = new Cat();
+
+cat.speakLater(1000); // meow
+```
--- 2-Module/2-week/2-day/scratch.js
+++ 2-Module/2-week/2-day/scratch.js
@@ -1,51 +1,162 @@
-class Test {
-  testFunc(name, num) {
-    console.log(name, num)
-    console.log(this === test1);
-  }
-}
+// class Cat {
+//     speak(sound = "meow") {
+//         console.log(this);
+//         console.log(sound);
+//     }
 
-const test1 = new Test();
+//     speakSpecific(sound) {
+//         // console.log(this);
+//         this.speak(sound);
+//     }
 
-// setTimeout(test1.testFunc.bind(test1, "Mylo", 3), 1000); // wait 1 second, true
-
-/* 
-Using this in Function Declaration Syntax
-Could I make `this` equal to test1 in this function?
-*/
-// function someFunc() {
-//   console.log(this);
+//     constructor(name) {
+//         this.name = name;
+//     }
 // }
 
-// someFunc.call(test1); // (undefined) Test {...}
+// let cat = new Cat("cat");
+// let otherCat = new Cat("otherCat");
 
+// cat.speak(); // 'meow'
+// cat.speakSpecific("ayyeee lmao"); // 'ayyeee lmao'
 
+// otherCat.speak(); // 'meow'
+// otherCat.speakSpecific("ayyeee lmao"); // 'ayyeee lmao'
 
+// const mySpeak = cat.speak;
+// const mySpecific = cat.speakSpecific;
 
-// function greeting(...messages) {
-//   return messages.map((message) => {
-//     return `${this.firstName} says ${message}`;
-//   });
-// }
+// console.log(mySpeak); // [Function: speak]
+// console.log(mySpecific); // [Function: speakSpecific]
 
-// const derek = {
-//   firstName: 'Derek',
+// mySpeak("still works"); // 'still works'
+// mySpecific("wait what happened"); // TypeError
+
+// let cat = new Cat("cat");
+
+// const mySpecific = cat.speakSpecific;
+// // const boundSpecific = mySpecific.bind(cat);
+
+// // console.log(boundSpecific); // [Function: bound speakSpecific]
+// // boundSpecific("ggez"); // ggez
+
+// const boundForever = mySpecific.bind(cat, "I'll never die");
+// boundForever("hey");
+// boundForever("ggez");
+
+// const printAString = function (someString) {
+//     console.log(someString);
 // };
 
-// const derekMessages = greeting.bind(derek, 'Hello class!');
-// console.log(derekMessages('Goodbye class!'));
+// const printZaviar = printAString.bind("lets not pass it a context", "Zaviar");
+// printZaviar();
 
-class Fruit {
-  constructor(name) {
-    this.name = name;
-  }
+// printAString("Kirin");
+// printAString();
 
-  eat = () => {
-    console.log(`${this.name} has a bite mark`);
-  };
-}
+// printAString("Zaviar");
 
-const apple = new Fruit('apple');
-const eat = apple.eat;
-eat();
-setTimeout(eat, 1000);
+// const printTwoNumbers = function (firstNum, secondNum) {
+//     console.log(this);
+//     console.log(firstNum, secondNum);
+// };
+
+// const alwaysStartWith1 = printTwoNumbers.bind(
+//     "literally any placeholder",
+//     "other args",
+//     "and other more different args"
+// );
+
+// alwaysStartWith1(32);
+// alwaysStartWith1(100);
+// alwaysStartWith1(50);
+// alwaysStartWith1("whatever");
+
+// printTwoNumbers(10, 32);
+
+// const myArr = [1, 2, 3];
+
+// const [,, three] = myArr;
+
+// console.log(three);
+
+// let cat = new Cat("cat");
+
+// const mySpecific = cat.speakSpecific;
+// mySpecific.call(cat); // meow
+// mySpecific.apply(cat); // meow
+
+// mySpecific.call(cat, "grrrrr"); // grrrrr
+// mySpecific.apply(cat, ["hisssss"]); // hisssss
+
+// const mySentenceJoin = function (arr) {
+//     console.log(arr);
+//     // console.log(arr.join(" "));
+// };
+
+// const andThisIsCrazy = ["hey", "i", "just", "met", "you"];
+// const andObjectCrazy = {
+//     hey: "hey",
+//     i: "i",
+//     just: "just",
+//     met: "met",
+//     you: "you",
+// };
+
+// mySentenceJoin(["hey", "i", "just", "met", "you"]);
+// mySentenceJoin("hey", "i", "just", "met", "you");
+
+// const newCallVersion = mySentenceJoin.call(null, andThisIsCrazy);
+// const newApplyVersion = mySentenceJoin.apply(null, andObjectCrazy);
+
+// class Cat {
+//     speak(sound = "meow") {
+//         console.log(sound);
+//     }
+
+//     speakSpecific(sound) {
+//         this.speak(sound);
+//     }
+
+//     speakLater(delay) {
+//         setTimeout(function () {
+//             console.log(this);
+//             this.speak();
+//         }, delay);
+//     }
+// }
+// let cat = new Cat();
+
+// cat.speakLater(1000); // TypeError
+
+// class Cat {
+//     speak(sound = "meow") {
+//         console.log(sound);
+//     }
+
+//     speakSpecific(sound) {
+//         this.speak(sound);
+//     }
+
+//     speakLater(delay) {
+//         setTimeout(() => {
+//             console.log(this);
+//             this.speak();
+//         }, delay);
+//     }
+
+//     constructor(name) {
+//         this.name = name;
+//     }
+// }
+// let cat = new Cat("cat");
+
+// cat.speakLater(1000); // meow
+
+// const myCoolFunc = function () {
+//     // this.name = "Zaviar";
+//     console.log(this);
+// };
+
+// // new myCoolFunc();
+// myCoolFunc();
