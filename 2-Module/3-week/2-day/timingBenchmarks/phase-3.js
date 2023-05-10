2cee46a8ed32dca170f73298961c31d1aaf16003
Zaviar Brown <brown.zaviar@gmail.com>
2023-05-09 17:52:57 -0700

    Final timing notes

--- 2-Module/3-week/2-day/timingBenchmarks/phase-3.js
+++ 2-Module/3-week/2-day/timingBenchmarks/phase-3.js
@@ -1,29 +1,47 @@
 const [addNums, addManyNums] = require("./phase-1");
 
 function addNums10Timing(increment) {
-  // Copy your `addNums10` code here
-  // Then, add timing code
+    const finalArr = [];
 
-  // Your code here
+    for (let i = 1; i <= 10; i++) {
+        const newVal = i * increment;
 
+        // let startTime = Date.now();
+        console.time("addNums");
+
+        finalArr.push(addNums(newVal));
+
+        console.timeEnd("addNums");
+        // let endTime = Date.now();
+
+        // console.log(endTime - startTime);
+    }
+
+    return finalArr;
 }
-
 
 function addManyNums10Timing(increment) {
-// Copy your `addManyNums10` code here
-// Then, add timing code
+    const finalArr = [];
+    const start = increment;
+    const end = increment * 10;
 
-  // Your code here
+    for (let i = start; i <= end; i += increment) {
+        console.time("addManyNums");
 
+        finalArr.push(addManyNums(i));
+
+        console.timeEnd("addManyNums");
+    }
+
+    return finalArr;
 }
 
-
-n = 1000000
+n = 1000000;
 console.log(`addNums(${n}): `);
 addNums10Timing(1000000);
 
 console.log("\n***********\n");
 
-n = 1000
+n = 5000;
 console.log(`addManyNums(${n}): `);
 addManyNums10Timing(5000);
