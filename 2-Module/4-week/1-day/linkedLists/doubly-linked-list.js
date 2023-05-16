1032ab0f52cb9e0cb49303f2e8ca27b0711dc51f
Zaviar Brown <brown.zaviar@gmail.com>
2023-05-15 18:06:51 -0700

    Full solution

--- 2-Module/4-week/1-day/linkedLists/doubly-linked-list.js
+++ 2-Module/4-week/1-day/linkedLists/doubly-linked-list.js
@@ -1,35 +1,101 @@
 class DoublyLinkedListNode {
-  constructor(val) {
-    this.value = val;
-    this.prev = null;
-    this.next = null;
-  }
+    constructor(val) {
+        this.value = val;
+        this.prev = null;
+        this.next = null;
+    }
 }
 
 class DoublyLinkedList {
-  constructor() {
-    // Your code here
-  }
-
-  addToHead(val) {
-    // Your code here
-  }
-
-  addToTail(val) {
-    // Your code here
-  }
-
-  // You can use this function to help debug
-  print() {
-    let current = this.head;
-
-    while (current) {
-      process.stdout.write(`${current.value} <-> `);
-      current = current.next;
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
     }
 
-    console.log("NULL");
-  }
+    addToHead(val) {
+        const newNode = new DoublyLinkedListNode(val);
+
+        if (!this.head) {
+            this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head.prev = newNode;
+        }
+
+        this.head = newNode;
+        this.length++;
+    }
+
+    addToTail(val) {
+        const newNode = new DoublyLinkedListNode(val);
+
+        if (!this.tail) {
+            this.head = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+        }
+
+        this.tail = newNode;
+        this.length++;
+    }
+
+    //? Less dry
+    // addToHead(val) {
+    //     const newNode = new DoublyLinkedListNode(val);
+
+    //     if (!this.head) {
+    //         this.head = newNode;
+    //         this.tail = newNode;
+    //     } else {
+    //         newNode.next = this.head;
+    //         this.head.prev = newNode;
+    //         this.head = newNode;
+    //     }
+
+    //     this.length++;
+    // }
+
+    //? Less dry
+    // addToTail(val) {
+    //     const newNode = new DoublyLinkedListNode(val);
+
+    //     if (!this.tail) {
+    //         this.head = newNode;
+    //         this.tail = newNode;
+    //     } else {
+    //         this.tail.next = newNode;
+    //         newNode.prev = this.tail;
+    //         this.tail = newNode;
+    //     }
+
+    //     this.length++;
+    // }
+
+    //! Kind of ugly
+    // addToTail(val) {
+    //     const newNode = new DoublyLinkedListNode(val);
+
+    //     if (!this.tail) return this.addToHead(val);
+
+    //     this.tail.next = newNode;
+    //     newNode.prev = this.tail;
+    //     this.tail = newNode;
+    //     this.length++;
+    // }
+
+    // You can use this function to help debug
+    print() {
+        let current = this.head;
+
+        while (current) {
+            process.stdout.write(`${current.value} <-> `);
+            current = current.next;
+        }
+
+        console.log("NULL");
+    }
 }
 
 module.exports = DoublyLinkedList;
