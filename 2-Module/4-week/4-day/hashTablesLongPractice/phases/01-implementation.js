b8255bd02d0243258e777332dc442091a685e357
Zaviar Brown <brown.zaviar@gmail.com>
2023-05-18 17:54:27 -0700

    Full solutions

--- 2-Module/4-week/4-day/hashTablesLongPractice/phases/01-implementation.js
+++ 2-Module/4-week/4-day/hashTablesLongPractice/phases/01-implementation.js
@@ -1,53 +1,111 @@
 class KeyValuePair {
-  constructor(key, value) {
-    this.key = key;
-    this.value = value;
-    this.next = null;
-  }
+    constructor(key, value) {
+        this.key = key;
+        this.value = value;
+        this.next = null;
+    }
 }
 
-class HashTable { // get O(1), set O(1), deleteKey O(1)
+class HashTable {
+    // get O(1), set O(1), deleteKey O(1)
 
-  constructor(numBuckets = 8) {
-    // Initialize your buckets here
-    // Your code here
-  }
-
-  hash(key) {
-    let hashValue = 0;
-
-    for (let i = 0; i < key.length; i++) {
-      hashValue += key.charCodeAt(i);
+    constructor(numBuckets = 8) {
+        this.count = 0;
+        this.capacity = numBuckets;
+        this.data = new Array(numBuckets).fill(null);
     }
 
-    return hashValue;
-  }
+    hash(key) {
+        let hashValue = 0;
 
-  hashMod(key) {
-    // Get index after hashing
-    return this.hash(key) % this.capacity;
-  }
+        for (let i = 0; i < key.length; i++) {
+            hashValue += key.charCodeAt(i);
+        }
 
+        return hashValue;
+    }
 
-  insert(key, value) {
-    // Your code here
-  }
+    hashMod(key) {
+        // Get index after hashing
+        return this.hash(key) % this.capacity;
+    }
 
+    insert(key, value) {
+        if (this.count / this.capacity > 0.7) this.resize();
 
-  read(key) {
-    // Your code here
-  }
+        const newData = new KeyValuePair(key, value);
 
+        const index = this.hashMod(key);
 
-  resize() {
-    // Your code here
-  }
+        if (!this.data[index]) {
+            this.data[index] = newData;
+        } else {
+            let currData = this.data[index];
 
+            while (currData) {
+                if (currData.key === key) {
+                    currData.value = value;
+                    return;
+                }
 
-  delete(key) {
-    // Your code here
-  }
+                currData = currData.next;
+            }
+
+            newData.next = this.data[index];
+
+            this.data[index] = newData;
+        }
+
+        this.count++;
+    }
+
+    read(key) {
+        let find = this.data[this.hashMod(key)];
+
+        if (find) {
+            while (find) {
+                if (find.key === key) return find.value;
+
+                find = find.next;
+            }
+        }
+    }
+
+    resize() {
+        const oldData = this.data;
+        this.capacity *= 2;
+        this.data = new Array(this.capacity).fill(null);
+        this.count = 0;
+
+        oldData.forEach((el) => {
+            while (el) {
+                this.insert(el.key, el.value);
+                el = el.next;
+            }
+        });
+    }
+
+    delete(key) {
+        let find = this.data[this.hashMod(key)];
+
+        if (find) {
+            if (find.key === key) {
+                this.data[this.hashMod(key)] = find.next;
+                this.count--;
+            } else {
+                while (find.next) {
+                    if (find.next.key === key) {
+                        find.next = find.next.next;
+                        this.count--;
+                        return;
+                    }
+
+                    find = find.next;
+                }
+            }
+        }
+        return "Key not found";
+    }
 }
-
 
 module.exports = HashTable;
--- 2-Module/4-week/4-day/hashTablesLongPractice/phases/02-practice-problems.js
+++ 2-Module/4-week/4-day/hashTablesLongPractice/phases/02-practice-problems.js
@@ -50,26 +50,54 @@ function anagrams(str1, str2) {
 //         }
 //     }
 
-
 //     return commons;
 // }
 
 function commonElements(arr1, arr2) {
     const firstSet = new Set(arr1);
-    
+
     return arr2.filter((num) => firstSet.has(num));
 }
 
 function duplicate(arr) {
-    // Your code here
+    const noDupes = new Set();
+
+    for (const num of arr) {
+        if (!noDupes.has(num)) noDupes.add(num);
+        else return num;
+    }
 }
 
 function twoSum(nums, target) {
-    // Your code here
+    const otherNums = new Set();
+
+    for (const num of nums) {
+        const theOtherNum = target - num;
+
+        if (otherNums.has(theOtherNum)) return true;
+        else otherNums.add(num);
+    }
+
+    return false;
 }
 
 function wordPattern(pattern, strings) {
-    // Your code here
+    const pairs = {};
+    const seenWords = new Set();
+
+    for (let i = 0; i < pattern.length; i++) {
+        const letter = pattern[i];
+        const word = strings[i];
+
+        if (!pairs[letter]) {
+            if (seenWords.has(word)) return false;
+
+            pairs[letter] = word;
+            seenWords.add(word);
+        } else if (pairs[letter] !== word) return false;
+    }
+
+    return true;
 }
 
 module.exports = [anagrams, commonElements, duplicate, twoSum, wordPattern];
