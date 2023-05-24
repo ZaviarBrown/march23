5248cb2ab47d559f47b711e2b07192140de10586
Zaviar Brown <brown.zaviar@gmail.com>
2023-05-23 17:21:51 -0700

    Tree lecture and 1st project solution

---
+++ 2-Module/5-week/2-day/1_binarySearch/binary-search.js
@@ -0,0 +1,37 @@
+function linearSearch(arr, target) {
+    return arr.indexOf(target);
+    // return arr.includes(target) ? target : -1;
+}
+
+function binarySearch(arr, target) {
+    // Set integers pointing to the high and low range of possible indices
+    let low = 0;
+    let high = arr.length - 1;
+
+    // While high and low indices do not overlap...
+    while (low <= high) {
+        // Find the midpoint between high and low indices
+        const mid = Math.floor((low + high) / 2);
+
+        // Compare the target value to the midpoint value
+        // If the target equals the midpoint...
+        if (target === arr[mid]) {
+            // Return the midpoint index
+            return mid;
+        }
+        // If the target is higher than the midpoint...
+        // Move the low pointer to midpoint + 1
+        if (target > arr[mid]) {
+            low = mid + 1;
+        } else {
+            // If the target is less than the midpoint...
+            // Move the high pointer to midpoint - 1
+            high = mid - 1;
+        }
+    }
+
+    // Return -1 if the loop exits with overlapping pointers
+    return -1;
+}
+
+module.exports = [linearSearch, binarySearch];
