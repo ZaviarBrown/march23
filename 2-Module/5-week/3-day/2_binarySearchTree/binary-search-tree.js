c527f563c2ef4e20868c8c559be07c0801591123
Zaviar Brown <brown.zaviar@gmail.com>
2023-05-24 21:49:38 -0700

    BST solutions

--- 2-Module/5-week/3-day/2_binarySearchTree/binary-search-tree.js
+++ 2-Module/5-week/3-day/2_binarySearchTree/binary-search-tree.js
@@ -95,26 +95,59 @@ class BinarySearchTree {
     }
 
     preOrderTraversal(currentNode = this.root) {
-        // Your code here
+        if (!currentNode) return null;
+
+        console.log(currentNode.val);
+        this.preOrderTraversal(currentNode.left);
+        this.preOrderTraversal(currentNode.right);
     }
 
     inOrderTraversal(currentNode = this.root) {
-        // Your code here
+        if (!currentNode) return null;
+
+        this.inOrderTraversal(currentNode.left);
+        console.log(currentNode.val);
+        this.inOrderTraversal(currentNode.right);
     }
 
     postOrderTraversal(currentNode = this.root) {
-        // Your code here
+        if (!currentNode) return null;
+
+        this.postOrderTraversal(currentNode.left);
+        this.postOrderTraversal(currentNode.right);
+        console.log(currentNode.val);
     }
 
     // Breadth First Traversal - Iterative
     breadthFirstTraversal() {
-        // your code here
+        const queue = [this.root];
+
+        while (queue.length) {
+            const currNode = queue.shift();
+
+            console.log(currNode.val);
+
+            currNode.left && queue.push(currNode.left);
+            currNode.right && queue.push(currNode.right);
+        }
     }
 
     // Depth First Traversal - Iterative
     depthFirstTraversal() {
-        // your code here
+        const stack = [this.root];
+
+        while (stack.length) {
+            const currNode = stack.pop();
+
+            console.log(currNode.val);
+
+            currNode.left && stack.push(currNode.left);
+            currNode.right && stack.push(currNode.right);
+        }
     }
+
+    // if (currNode.left) queue.push(currNode.left);
+    // if (currNode.right) queue.push(currNode.right);
 }
 
 module.exports = { BinarySearchTree, TreeNode };
