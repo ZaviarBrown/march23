400bd2325c03cb9bdd75241ab96db959a0aac493
Zaviar Brown <brown.zaviar@gmail.com>
2023-07-11 18:07:13 -0700

    Project solutions & DOM lecture notes

---
+++ 3-Module/5-week/2-day/2_es6/assets/js/game.js
@@ -0,0 +1,45 @@
+import { getIndex } from "./utilities.js";
+// import { mrPotatoHeadQuotes } from "./quotes/mrPotatoHead.js";
+// import mrsPotatoHeadQuotes from "./quotes/mrsPotatoHead.js";
+import quoteCity from "./quotes/index.js";
+
+// export default class Game {
+export class Game {
+    start() {
+        document.getElementById("hello").addEventListener("click", () => {
+            const index = getIndex();
+            const messageContainer = document.getElementById("message");
+            if (index === 1) {
+                // messageContainer.innerText = mrPotatoHeadQuotes["hello"];
+                messageContainer.innerText = quoteCity.mr["hello"];
+            } else {
+                messageContainer.innerText = quoteCity.mrs["hello"];
+            }
+        });
+
+        document.getElementById("bye").addEventListener("click", () => {
+            const index = getIndex();
+            const messageContainer = document.getElementById("message");
+            if (index === 1) {
+                messageContainer.innerText = quoteCity.mr["bye"];
+            } else {
+                messageContainer.innerText = quoteCity.mrs["bye"];
+            }
+        });
+
+        document.getElementById("swap").addEventListener("click", () => {
+            const index = getIndex();
+            const image = document.getElementById("image");
+            const messageContainer = document.getElementById("message");
+            const wrapper = document.getElementById("wrapper");
+            if (index === 1) {
+                image.src = "./assets/images/potatohead2.png";
+                wrapper.dataset.index = "2";
+            } else {
+                image.src = "./assets/images/potatohead1.png";
+                wrapper.dataset.index = "1";
+            }
+            messageContainer.innerText = "";
+        });
+    }
+}
---
+++ 3-Module/5-week/2-day/2_es6/assets/js/quotes/index.js
@@ -0,0 +1,14 @@
+import { mrPotatoHeadQuotes } from "./mrPotatoHead.js";
+import mrsPotatoHead from "./mrsPotatoHead.js";
+
+// const allQuotes = {
+//     mr: mrPotatoHeadQuotes,
+//     mrs: mrsPotatoHead,
+// };
+
+// export default allQuotes;
+
+export default {
+    mr: mrPotatoHeadQuotes,
+    mrs: mrsPotatoHead,
+};
