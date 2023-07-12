af6047610b4b9426be45ec8dd3b090f8d6e08b9d
Zaviar Brown <brown.zaviar@gmail.com>
2023-07-11 22:01:11 -0700

    Project solutions & DOM lecture notes

---
+++ 3-Module/5-week/2-day/2_es6/assets/js/quotes/index.js
@@ -0,0 +1,7 @@
+import { mrPotatoHeadQuotes } from "./mrPotatoHead.js";
+import mrsPotatoHead from "./mrsPotatoHead.js";
+
+export default {
+    mr: mrPotatoHeadQuotes,
+    mrs: mrsPotatoHead,
+};
