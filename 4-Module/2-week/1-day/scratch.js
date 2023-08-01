568444f88adb57a5f5a2c38543378d39a800a80e
Zaviar Brown <brown.zaviar@gmail.com>
2023-07-31 22:01:08 -0700

    Sqlite intro content

---
+++ 4-Module/2-week/1-day/scratch.js
@@ -0,0 +1,14 @@
+const user = {
+    id: 1,
+    email: "zb@aa.io",
+    username: "ZavBro",
+};
+
+class UserData {
+    constructor(id, email, username) {
+        this.id = id;
+        this.email = email;
+        this.username = username;
+    }
+}
+
