d2d00f1fa3629c949f1b91243bb55a34fd3e6720
Zaviar Brown <brown.zaviar@gmail.com>
2023-08-09 18:43:12 -0700

    All Wednesday Sequelize practice solutions

---
+++ 4-Module/3-week/3-day/solutions/5_sequelize-seeders/server/db/seeders/20230810004432-primary-colors.js
@@ -0,0 +1,35 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        /**
+         * Add seed commands here.
+         *
+         * Example:
+         * await queryInterface.bulkInsert('People', [{
+         *   name: 'John Doe',
+         *   isBetaMember: false
+         * }], {});
+         */
+
+        await queryInterface.bulkInsert("Colors", [
+            { name: "red" },
+            { name: "blue" },
+            { name: "yellow" },
+        ]);
+    },
+
+    async down(queryInterface, Sequelize) {
+        /**
+         * Add commands to revert seed here.
+         *
+         * Example:
+         * await queryInterface.bulkDelete('People', null, {});
+         */
+        await queryInterface.bulkDelete("Colors", {
+            name: ["red", "blue", "yellow"],
+        });
+
+    },
+};
