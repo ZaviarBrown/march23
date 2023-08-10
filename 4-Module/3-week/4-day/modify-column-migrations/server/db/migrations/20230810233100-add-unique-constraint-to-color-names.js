c7d7131b494b4a6c445a4d4b7ec3b181b3c5fdac
Zaviar Brown <brown.zaviar@gmail.com>
2023-08-10 16:53:02 -0700

    Modify column solutions

---
+++ 4-Module/3-week/4-day/modify-column-migrations/server/db/migrations/20230810233100-add-unique-constraint-to-color-names.js
@@ -0,0 +1,30 @@
+"use strict";
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        /**
+         * Add altering commands here.
+         *
+         * Example:
+         * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+         */
+        await queryInterface.addConstraint("Colors", {
+            type: "unique",
+            fields: ["name"],
+            name: "unique-name-for-colors",
+        });
+    },
+
+    down: async (queryInterface, Sequelize) => {
+        /**
+         * Add reverting commands here.
+         *
+         * Example:
+         * await queryInterface.dropTable('users');
+         */
+        await queryInterface.removeConstraint(
+            "Colors",
+            "unique-name-for-colors"
+        );
+    },
+};
