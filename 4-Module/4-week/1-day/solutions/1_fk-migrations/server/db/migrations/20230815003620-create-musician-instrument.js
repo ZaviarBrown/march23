d8122319cee4712900cf4966e1e341ebdca3c556
Zaviar Brown <brown.zaviar@gmail.com>
2023-08-14 17:58:11 -0700

    FK migration solutions

---
+++ 4-Module/4-week/1-day/solutions/1_fk-migrations/server/db/migrations/20230815003620-create-musician-instrument.js
@@ -0,0 +1,36 @@
+"use strict";
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable("MusicianInstruments", {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            musicianId: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: { model: "Musicians" },
+            },
+            instrumentId: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: { model: "Instruments" },
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+            },
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable("MusicianInstruments");
+    },
+};
