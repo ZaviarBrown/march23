d9d9e5ae1ea99a710283e6cae04feec69ea678b0
Zaviar Brown <brown.zaviar@gmail.com>
2023-08-22 20:52:45 -0700

    Polymorphic Associations solution

---
+++ 4-Module/5-week/2-day/sequelize-polymorphism/server/db/models/image.js
@@ -0,0 +1,51 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+    class Image extends Model {
+        getImageable(options) {
+            if (!this.imageableType) return Promise.resolve(null);
+            // if (!this.imageableType) return Promise['resolve'](null);
+
+            const myCustomFunctionName = `get${this.imageableType}`; // getBlogPost OR getUserProfile
+            return this[myCustomFunctionName](options);
+        }
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            Image.belongsTo(models.UserProfile, {
+                foreignKey: "imageableId",
+                constraints: false,
+            });
+            Image.belongsTo(models.BlogPost, {
+                foreignKey: "imageableId",
+                constraints: false,
+            });
+        }
+    }
+    Image.init(
+        {
+            url: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            imageableType: {
+                type: DataTypes.ENUM("UserProfile", "BlogPost"),
+            },
+            imageableId: DataTypes.INTEGER,
+        },
+        {
+            sequelize,
+            modelName: "Image",
+        }
+    );
+    return Image;
+};
+
+// const myImage = new Image("http://coolimage.jpg", "BlogPost", 5);
+
+// console.log(myImage.imageableType);
+
+// myImage.getImageable()
