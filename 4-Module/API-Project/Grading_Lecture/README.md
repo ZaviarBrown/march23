491ef019c87a732fc9d808908f7e2f9e260cbbcb
Zaviar Brown <brown.zaviar@gmail.com>
2023-09-09 14:43:27 -0700

    Start of grading demo

---
+++ 4-Module/API-Project/Grading_Lecture/README.md
@@ -0,0 +1,24 @@
+# Grading Requirements
+
+## Merge your own PR's
+
+If you've been creating PR's and waiting for us to merge them, it's now time to be free 🕊️
+
+## Reiterating "Exactly"
+
+Seed data values do not need to match
+
+Error values **_`Need To Match Exactly`_**
+
+Extra error keys will pass, but you should aim to match the docs exactly
+
+Week 19 Wednesday has useful readings and practices on error handling
+
+## Think you're done? Ask for a pre-grade
+
+Once you have tested everything on production to the best of your ability, you may ask us to pre-grade your project
+
+- One pre-grade per student
+- You'll receive feedback on what needs to be fixed
+- Does not count toward your final grade
+  - Don't break something after a successful pre-grade!
---
+++ 4-Module/API-Project/Grading_Lecture/starter/backend/routes/api/users.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const { check } = require("express-validator");
+const bcrypt = require("bcryptjs");
+
+const { handleValidationErrors } = require("../../utils/validation");
+const { setTokenCookie, requireAuth } = require("../../utils/auth");
+const { User } = require("../../db/models");
+
+const router = express.Router();
+
+const validateSignup = [
+    check("email")
+        .exists({ checkFalsy: true })
+        .isEmail()
+        .withMessage("Invalid email"),
+    check("username")
+        .exists({ checkFalsy: true })
+        .isLength({ min: 4 })
+        .withMessage("Please provide a username with at least 4 characters."),
+    check("username")
+        .not()
+        .isEmail()
+        .withMessage("Username cannot be an email."),
+    check("password")
+        .exists({ checkFalsy: true })
+        .isLength({ min: 6 })
+        .withMessage("Password must be 6 characters or more."),
+    handleValidationErrors,
+];
+
+// Sign up
+router.post("/", validateSignup, async (req, res) => {
+    const { email, password, username, firstName, lastName } = req.body;
+    const hashedPassword = bcrypt.hashSync(password);
+    const user = await User.create({
+        email,
+        username,
+        hashedPassword,
+        firstName,
+        lastName,
+    });
+
+    const safeUser = {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+        firstName: user.firstName,
+        lastName: user.lastName,
+    };
+
+    await setTokenCookie(res, safeUser);
+
+    return res.json({
+        user: safeUser,
+    });
+});
+
+module.exports = router;
