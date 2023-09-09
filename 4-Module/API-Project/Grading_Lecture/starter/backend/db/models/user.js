d7d2641b2a5a7086d8df806866c5ec3c1ddfa10c
Zaviar Brown <brown.zaviar@gmail.com>
2023-09-09 14:41:33 -0700

    Start of grading demo

---
+++ 4-Module/API-Project/Grading_Lecture/starter/backend/db/models/user.js
@@ -0,0 +1,67 @@
+"use strict";
+const { Model, Validator } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+    class User extends Model {
+        static associate(models) {
+            // define association here
+        }
+    }
+
+    User.init(
+        {
+            firstName: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            lastName: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            username: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true,
+                validate: {
+                    len: [4, 30],
+                    isNotEmail(value) {
+                        if (Validator.isEmail(value)) {
+                            throw new Error("Cannot be an email.");
+                        }
+                    },
+                },
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true,
+                validate: {
+                    len: [3, 256],
+                    isEmail: true,
+                },
+            },
+            hashedPassword: {
+                type: DataTypes.STRING.BINARY,
+                allowNull: false,
+                validate: {
+                    len: [60, 60],
+                },
+            },
+        },
+        {
+            sequelize,
+            modelName: "User",
+            defaultScope: {
+                attributes: {
+                    exclude: [
+                        "hashedPassword",
+                        "email",
+                        "createdAt",
+                        "updatedAt",
+                    ],
+                },
+            },
+        }
+    );
+    return User;
+};
