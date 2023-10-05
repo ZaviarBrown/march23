32c2189670ac939ccc518b39b4ff2a4101d1330b
Zaviar Brown <brown.zaviar@gmail.com>
2023-10-04 19:58:17 -0700

    Redux lecture notes

---
+++ 5-Module/3-week/3-day/scratch.js
@@ -0,0 +1,14 @@
+const myFirstArray = [1, 2, 3];
+
+console.log([...myFirstArray, 4]);
+console.log(myFirstArray);
+
+const myFirstObj = {
+    a: "A",
+    b: "B",
+    c: "Y",
+};
+
+console.log(myFirstObj);
+console.log({ ...myFirstObj, c: "C" });
+console.log(myFirstObj);
